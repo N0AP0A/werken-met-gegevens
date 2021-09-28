13d742a4e65b7211cbb9d9d8e63e7cf600723d74
N0AP0A <99070686@mydavinci.nl>
2021-09-28 22:38:08 +0200

    enter

--- Feestlunch.py
+++ Feestlunch.py
@@ -3,6 +3,6 @@ aantalcroissantjes = 17
 stokbroden = 2.78
 aantalstokbroden = 2
 aantalkortingsbonnen =3
-kortingsbonnen = 0.53
+kortingsbonnen = 0.60 
 
 print(croissantjes * aantalcroissantjes + (stokbroden * aantalstokbroden) - aantalkortingsbonnen * kortingsbonnen)
