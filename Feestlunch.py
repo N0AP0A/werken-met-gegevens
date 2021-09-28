cb55b145a9f7569b923cf95c411772afb1656ac0
N0AP0A <99070686@mydavinci.nl>
2021-09-28 23:02:25 +0200

    enter

--- Feestlunch.py
+++ Feestlunch.py
@@ -4,5 +4,6 @@ stokbroden = 2.78
 aantalstokbroden = 2
 aantalkortingsbonnen =3
 kortingsbonnen = 0.20
+total = (croissantjes * aantalcroissantjes + stokbroden * aantalstokbroden - (aantalkortingsbonnen * kortingsbonnen))
 
-print(croissantjes * aantalcroissantjes + (stokbroden * aantalstokbroden) - aantalkortingsbonnen * kortingsbonnen)
+print ("De feestlunch kost je bij de bakke" , total , "euro voor de 17 croissantjes en de 2 stokbroden als de 3 kortingsbonnen nog geldig zijn")
