ea13e9f265b5c32346436d7a48e833d8b4e3e45b
N0AP0A <91467633+N0AP0A@users.noreply.github.com>
2021-09-28 12:05:40 +0200

    Add files via upload

---
+++ Feestlunch.py
@@ -0,0 +1,8 @@
+croissantjes = 0.39
+aantalcroissantjes = 17
+stokbroden = 2.78
+aantalstokbroden = 2
+aantalkortingsbonnen =3
+kortingsbonnen = 0.50
+
+print(croissantjes * aantalcroissantjes + (stokbroden * aantalstokbroden) - aantalkortingsbonnen * kortingsbonnen)
---
+++ deSpeelhal-dag.py
@@ -0,0 +1,5 @@
+toegangsticket = 7.45
+minuten = 0.37 
+personen= 3
+
+print (toegangsticket * personen + (minuten * personen))
