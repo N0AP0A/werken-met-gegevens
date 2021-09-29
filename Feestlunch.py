e262c039ad8f6c2ed2ab741eb6f048f82e8ec58c
N0AP0A <99070686@mydavinci.nl>
2021-09-29 09:08:50 +0200

    enter

--- Feestlunch.py
+++ Feestlunch.py
@@ -6,4 +6,4 @@ aantalkortingsbonnen =3
 kortingsbonnen = 0.20
 total = (croissantjes * aantalcroissantjes + stokbroden * aantalstokbroden - (aantalkortingsbonnen * kortingsbonnen))
 
-print ("De feestlunch kost je bij de bakke" , total , "euro voor de 17 croissantjes en de 2 stokbroden als de 3 kortingsbonnen nog geldig zijn")
+print("De feestlunch kost je bij de bakke" , total , "euro voor de 17 croissantjes en de 2 stokbroden als de 3 kortingsbonnen nog geldig zijn")
--- deSpeelhal-dag.py
+++ deSpeelhal-dag.py
@@ -2,4 +2,4 @@ toegangsticket = 7.45
 minuten = 0.37 
 personen= 3
 
-print (toegangsticket * personen + (minuten * personen))
+print   (toegangsticket * personen + (minuten * personen))
